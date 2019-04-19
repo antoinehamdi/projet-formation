1be89501ca17a55848dd8598de600858cc3386e7
ninjasimov <lafayettelothaire@yahoo.fr>
2019-04-19 02:46:12 -0700

    ajout script getArtifact.sh avec set up des variables de base

---
+++ getArtifact.sh
@@ -0,0 +1,8 @@
+ #wget http://192.168.100.74:8081/repository/maven-central/org/codehaus/plexus/plexus-interpolation/1.14/plexus-interpolation-1.14.pom.sha1
+
+NEXUS_URL=192.168.100.74
+NEXUS_PORT=8081
+SNAPSHOT_REPOSITORY=maven-snapshots
+RELEASE_REPOSITORY=maven-releases
+NEXUS_LOGIN=admin
+NEXUS_PWD=admin123
