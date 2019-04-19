804270026663553245d7c61b1717a00d30f50761
root <root@localhost.localdomain>
2019-04-19 02:52:23 -0700

    verif version release or snapshot

--- getArtifact.sh
+++ getArtifact.sh
@@ -6,3 +6,12 @@ SNAPSHOT_REPOSITORY=maven-snapshots
 RELEASE_REPOSITORY=maven-releases
 NEXUS_LOGIN=admin
 NEXUS_PWD=admin123
+# Vérification du type de l'artifact passé en argument.
+
+if [[ $1 == *"-SNAPSHOT" ]]; then
+# version SNAPSHOT.
+	echo "Snapshot"
+else
+# defaut = version RELEASE.
+	echo "Release"
+fi
