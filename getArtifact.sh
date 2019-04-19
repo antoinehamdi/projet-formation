c3eb55c92bce2004eb9c7528a647890920f453b5
ninjasimov <lafayettelothaire@yahoo.fr>
2019-04-19 03:15:51 -0700

    recuperation par curl du downloadUrl dans le JSON

--- getArtifact.sh
+++ getArtifact.sh
@@ -6,6 +6,8 @@ SNAPSHOT_REPOSITORY=maven-snapshots
 RELEASE_REPOSITORY=maven-releases
 NEXUS_LOGIN=admin
 NEXUS_PWD=admin123
+NEXUS_AUTH=$NEXUS_LOGIN:$NEXUS_PWD
+NEXUS_SOCKET=$NEXUS_URL:$NEXUS_PORT
 # Vérification du type de l'artifact passé en argument.
 
 if [[ $1 == *"-SNAPSHOT" ]]; then
@@ -13,5 +15,10 @@ if [[ $1 == *"-SNAPSHOT" ]]; then
 	echo "Snapshot"
 else
 # defaut = version RELEASE.
-	echo "Release"
+TRUC="${NEXUS_SOCKET}/service/rest/v1/search/assets?repository=${RELEASE_REPOSITORY}&name=server&maven.extension=jar&version=$1"
+echo $TRUC
+DL_URL=$(curl -u ${NEXUS_AUTH} -X GET $TRUC \
+)
+echo $DL_URL
 fi
+
