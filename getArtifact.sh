05ea7d4caafefb6a9ea71959f74ffc7a034d2b29
ninjasimov <lafayettelothaire@yahoo.fr>
2019-04-19 05:20:42 -0700

    ajout récuperation latest version snapshot

--- getArtifact.sh
+++ getArtifact.sh
@@ -8,17 +8,23 @@ NEXUS_LOGIN=admin
 NEXUS_PWD=admin123
 NEXUS_AUTH=$NEXUS_LOGIN:$NEXUS_PWD
 NEXUS_SOCKET=$NEXUS_URL:$NEXUS_PORT
+MAVEN_DATA_URL=$NEXUS_SOCKET/repository/maven-snapshots/com/lesformateurs/maven-project/server/$1/maven-metadata.xml
+echo $MAVEN_DATA_URL
 # Vérification du type de l'artifact passé en argument.
 
 if [[ $1 == *"-SNAPSHOT" ]]; then
 # version SNAPSHOT.
-	echo "Snapshot"
+LATEST_SNAP=$(curl -X GET $MAVEN_DATA_URL | grep value | head -n 1 | grep -Po "[0-9-.]*")
+echo $LATEST_SNAP
+API_SNAPSHOT_INFO="${NEXUS_SOCKET}/service/rest/v1/search/assets?repository=${SNAPSHOT_REPOSITORY}&name=server&maven.extension=jar&version=$1"
+INFO_SNAPSHOT=$(curl -u ${NEXUS_AUTH} -X GET $API_SNAPSHOT_INFO)
+DL_SNAPSHOT_URL=$(echo "$INFO_SNAPSHOT" | grep -Po "http://[0-9a-zA-Z.:/-]*" | grep "$1\.jar")
 else
 # defaut = version RELEASE.
-TRUC="${NEXUS_SOCKET}/service/rest/v1/search/assets?repository=${RELEASE_REPOSITORY}&name=server&maven.extension=jar&version=$1"
-echo $TRUC
-DL_URL=$(curl -u ${NEXUS_AUTH} -X GET $TRUC \
-)
-echo $DL_URL
+API_RELEASE_INFO="${NEXUS_SOCKET}/service/rest/v1/search/assets?repository=${RELEASE_REPOSITORY}&name=server&maven.extension=jar&version=$1"
+INFO_RELEASE=$(curl -u ${NEXUS_AUTH} -X GET $API_RELEASE_INFO)
+DL_RELEASE_URL=$(echo "$INFO_RELEASE" | grep -Po "http://[0-9a-zA-Z.:/-]*" | grep "$1\.jar")
+
 fi
 
+echo $DL_RELEASE_URL
